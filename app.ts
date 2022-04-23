f86c23950d05c38bfab1d40083e4102117e28bb8
Ji-hoon Lee <anteater1056@gmail.com>
2022-04-23 13:53:58 +0900

    기능 검증

--- app.ts
+++ app.ts
@@ -1,12 +1,27 @@
 import nodemailer from "nodemailer";
 
-console.log(
-  nodemailer.getTestMessageUrl({
-    envelope: undefined,
-    messageId: "",
-    response: "",
-    accepted: [],
-    rejected: [],
-    pending: [],
+const options = {
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user: "anteater1056@gmail.com",
+    pass: "exbesyrmjvtpcbdx",
+  },
+};
+
+const transporter = nodemailer.createTransport({
+  ...options,
+});
+
+const mailDestination = "zx1056@naver.com";
+transporter
+  .sendMail({
+    from: `"Que" no-reply-que@gmail.com`,
+    to: mailDestination,
+    subject: "QUE 인증메일",
+    text: "111111 입니다.",
   })
-);
+  .then((result) => {
+    console.log(result.response);
+  });
