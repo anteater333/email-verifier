aa15b413fa6260db1001ed3b14f967c46648d635
Ji-hoon Lee <anteater1056@gmail.com>
2022-06-28 01:56:38 +0900

    개발중 오류 수정을 위해 필요한 로그 보강

--- app.ts
+++ app.ts
@@ -5,6 +5,11 @@ import appConfig from "./src/config/config.ts";
 import router from "./src/controllers/router.ts";
 import Database from "./src/database/database.ts";
 
+console.log("Starting server");
+console.log("Server ENV list:");
+console.log(appConfig);
+console.log("======");
+
 const app = new Application();
 
 /** start db connection */
